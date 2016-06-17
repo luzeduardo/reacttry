ea0002b3208d33a21ec00a97fcb96577e146d56d
Eduardo Luz <luz.eduardo@gmail.com>
2016-06-17 08:26:30 -0300

    following the tutorial

--- public/component.js
+++ public/component.js
@@ -2,10 +2,28 @@ var CommentBox = React.createClass({
 	render: function(){
 		return (
 			<div className="commentBox">
-				Hello world! I am a comment box.
+				<h1>Comments</h1>
+				<CommenList />
+				<CommentForm />
 			</div>
 		);
 	}
 });
+
+var CommenList = React.createClass({
+	render: function(){
+		return (
+			<div className="CommenList">Hello World I am a CommenList</div>
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function(){
+		return(
+			<div className="commentForm">Hello World I am a CommentForm</div>
+		);
+	}
+}) 
 
 ReactDOM.render(<CommentBox/>, document.getElementById('content'));
