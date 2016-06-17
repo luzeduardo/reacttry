3ab0c4625f8f35cbd018a2b270bcfbe27a82b577
Eduardo Luz <luz.eduardo@gmail.com>
2016-06-16 22:33:43 -0300

    react try

---
+++ public/component.js
@@ -0,0 +1,11 @@
+var CommentBox = React.createClass({
+	render: function(){
+		return (
+			<div className="commentBox">
+				Hello world! I am a comment box.
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(<CommentBox/>, document.getElementById('content'));
