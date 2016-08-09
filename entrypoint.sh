b64cdbf703de3c1eff5a7f5c9f3353b953c4ae5b
Eduardo Luz <luz.eduardo@gmail.com>
2016-08-08 23:04:16 -0300

    dev config

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ USER_ID=${LOCAL_USER_ID:-9001}
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 export HOME=/home/user
-#webpack
-#npm start
-bash
+npm run dev
+#npm run build && npm start
+#bash
 
 #docker run -it -p 8000:8000 -v `pwd`:/usr/src/app -v `pwd`/entrypoint.sh:/usr/local/bin/entrypoint.sh -e LOCAL_USER_ID=`id -u $USER` --name node-container node-container
